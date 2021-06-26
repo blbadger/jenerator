28e974865be73678dae90081be6b947f8e56ab3e
blbadger <blbadger10@gmail.com>
2021-06-26 13:58:21 -0400

    fixed mset

--- app.py
+++ app.py
@@ -366,7 +366,7 @@ def disable_interval(img):
 
 # run the app in the cloud
 if __name__ == '__main__':
-	# app.run_server(debug=True, port=8067)
+	# app.run_server(debug=True, port=8068)
 	app.run_server(debug=True, host='0.0.0.0')
 
 
